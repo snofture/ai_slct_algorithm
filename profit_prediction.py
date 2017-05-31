5bb82ed9812642fb95a108998b5fba954f7ffdc3
unknown <limen@360buyad.local>
2017-05-31 16:48:55 +0800

    update profit_pred.py

--- profit_prediction.py
+++ profit_prediction.py
@@ -7,11 +7,11 @@ This is a temporary script file.
 
 #-*- coding=utf-8 -*-
 import numpy as np
-from numpy.random import randn
 import pandas as pd
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 from sklearn import metrics
+#import seaborn as sns
 
 #import attributes table
 attrs =  pd.read_excel('attrs.xlsx',sheetname='Sheet1', encoding = 'utf-8') 
@@ -28,6 +28,7 @@ a = a.reset_index(drop=False)
 #replace wrong information with correct data while transforming
 a.replace(42928,'7-12',inplace=True)
 a.replace(u'其他',u'其它',inplace=True)
+
 
 
 #import profit table
@@ -52,11 +53,20 @@ final_npp = pd.merge(a,average_profit, how = 'inner', on = 'sku_id')
 final_npp.drop('sku_id', axis = 1, inplace=True)
 
 
-#label encoder method to handle the categorical columns
-for attribute in final_npp.columns:
+#label encoder method to handle the categorical columns except the net_profit column
+for attribute in final_npp.columns.difference(['net_profit']):
     le = preprocessing.LabelEncoder()
     final_npp[attribute] = le.fit_transform(final_npp[attribute])
- 
+    
+#sns.distplot(final_npp['net_profit'] )
+
+
+#print(final_npp.loc[final_npp['net_profit'] < -1500000])
+#final_npp.to_csv('final_npp.csv', encoding = 'utf-8')
+final_npp.drop(final_npp.index[[39,52]],inplace = True)
+#print(final_npp.loc[final_npp['net_profit'] < -1700000])
+
+
 
 
 #train_test_split
@@ -64,11 +74,22 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(final_npp.drop('net_profit',axis=1), 
                                                     final_npp['net_profit'], 
                                                     test_size=0.30, 
-                                                    random_state=101)
-X_train = X_train.as_matrix()
-y_train = y_train.as_matrix()
-X_test = X_test.as_matrix()
+                                                    random_state = 101)
+#X_train = X_train.as_matrix()
+#y_train = y_train.as_matrix()
+#X_test = X_test.as_matrix()
 #y_test = y_test.as_matrix()
+
+
+
+'''
+#build linear regression model
+from sklearn.linear_model import LinearRegression
+lm = LinearRegression()
+lm.fit(X_train,y_train)
+predictions = lm.predict(X_test)
+'''
+
 
 
 #build random forest model
@@ -76,19 +97,19 @@ from sklearn.ensemble import RandomForestRegressor
   
 rfr = RandomForestRegressor(n_estimators = 100, 
                               max_features = 13,
-                              max_depth=5,
+                              max_depth=4,
                               #min_samples_split=4,
                               oob_score=True,
                               n_jobs=-1)
 rfr.fit(X_train, y_train)
 predictions = rfr.predict(X_test)
 
+
 #model evaluation
 plt.scatter(y_test,predictions)
 print('MAE:', metrics.mean_absolute_error(y_test, predictions))
 print('MSE:', metrics.mean_squared_error(y_test, predictions))
 print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
-
 
 
 
